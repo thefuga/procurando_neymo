c9bec86e53a52c87077f2d94401b07d07d236876
Erick Costa Fuga <erick_fuga@hotmail.com>
2018-07-15 20:57:11 -0300

    Adicionadas as constantes com código das mensagens.

--- consts.py
+++ consts.py
@@ -1,2 +1,14 @@
-DEFAULT_PORT = 1234
+#Constantes do servidor:
+DEFAULT_PORT = '1234'
 DEFAULT_NAME = 'localhost'
+
+#Identificadores de mensagems:
+MSG_OK = b'1'
+MSG_NOPE = b'2'
+ASK_HOST = b'3'
+ASK_ESP_OPP = b'4' 
+ASK_ANY_OPP = b'5'
+SRVR_ANSR = b'6'
+HND_SHK = b'7'
+HND_SHK_ANSR = b'8'
+
