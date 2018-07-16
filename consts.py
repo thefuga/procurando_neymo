2cb41e3804f205415357a2267ed6d2e225431a10
Erick Costa Fuga <erick_fuga@hotmail.com>
2018-07-15 23:35:47 -0300

    Modificado o conteúdo das mensagens de byte para inteiro.

--- consts.py
+++ consts.py
@@ -1,14 +1,14 @@
 #Constantes do servidor:
-DEFAULT_PORT = '1234'
+DEFAULT_PORT = 1234
 DEFAULT_NAME = 'localhost'
 
 #Identificadores de mensagems:
-MSG_OK = b'1'
-MSG_NOPE = b'2'
-ASK_HOST = b'3'
-ASK_ESP_OPP = b'4' 
-ASK_ANY_OPP = b'5'
-SRVR_ANSR = b'6'
-HND_SHK = b'7'
-HND_SHK_ANSR = b'8'
+MSG_OK = 1
+MSG_NOPE = 2
+ASK_HOST = 3
+ASK_ESP_OPP = 4 
+ASK_ANY_OPP = 5
+SRVR_ANSR = 6
+HND_SHK = 7
+HND_SHK_ANSR = 8
 
