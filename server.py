e0910cc6bfee894d5bd5a9bbf7934db678e18886
Erick Costa Fuga <erick_fuga@hotmail.com>
2018-07-16 00:38:46 -0300

    Server atualizado para lidar com stream de bytes

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ class Server(object):
         self.__port = consts.DEFAULT_PORT
         self.__name = consts.DEFAULT_NAME
         self.__socket = socket(AF_INET, SOCK_STREAM)
-
+        self.__hosts = []
 
     def init_server(self):
 
@@ -18,12 +18,22 @@ class Server(object):
         connection_socket, address = self.__socket.accept()
 
         while True:
-            query = connection_socket.recv(1024)#vai depender do protocolo. Pode tanto ser um nome de usuário ou uma palavra reservada para pegar um usuário qualquer.
+            query = connection_socket.recv(32)#
 
             if(query):
-                print(query.decode("utf-8"))
+                if(query[0:1].decode() == consts.ASK_HOST):
+                    #message = bytearray()
+                    print("ASK_HOST")
+                    self.__hosts.append((query[1:17].decode(), address))
+                    print(query[1:17].decode())
+                    connection_socket.send(consts.MSG_OK.encode())                    
+                elif(query[0:1].decode() == consts.ASK_ESP_OPP):
+                    print("ASK_ESP_OPP")
+                    connection_socket.send(query)                    
+                elif(query[0:1].decode() == consts.ASK_ANY_OPP):
+                    print("ASK_ANY_OPP")
+                    connection_socket.send(query)                    
 
-                connection_socket.send(query) #retorna o endereço do usuário encontrado ou erro. No momento, retornando a própria solicitação só pra testar.
                 connection_socket.close()
                 connection_socket, address = self.__socket.accept()
 
