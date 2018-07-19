7fb9cd908f1f38b07ed6c29c50141ec988e8314e
Erick Costa Fuga <erick_fuga@hotmail.com>
2018-07-18 22:48:02 -0300

    EXU CAVERINHA VENHA TRABALHAR

--- peer.py
+++ peer.py
@@ -6,21 +6,22 @@ import consts
 
 class ServerPeer(threading.Thread):
 
-    def __init__(self, my_port, my_ip):
+    def __init__(self):
         threading.Thread.__init__(self)
         self.__running = True
-        self.__my_port = my_port
-        self.__my_ip = my_ip
+        #self.__my_port = my_port
+        #self.__my_ip = my_ip
         self.__connection_socket = None
         self.__address = None
         
        
 
     def run(self):
-        
+        host = ''
+        port = 1776
         socket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
         socket.setsockopt(skt.SOL_SOCKET, skt.SO_REUSEADDR, 1)
-        socket.bind((self.__my_ip, self.__my_port))
+        socket.bind((host, port))
         socket.listen(1)
 
         self.__connection_socket, self.__address = socket.accept()
@@ -43,17 +44,19 @@ class ServerPeer(threading.Thread):
 
 class ClientPeer(threading.Thread):
 
-    def __init__(self, peer_port, peer_ip):
+    def __init__(self):
         threading.Thread.__init__(self)
         self.__running = 1
-        self.__peer_ip = peer_ip
-        self.__peer_port = peer_port
+        self.__peer_ip = None
+        self.__client_socket = None
+        #self.__peer_port = None
         
 
     def run(self):
 
+        port = 1776
         self.__client_socket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
-        self.__client_socket.connect((self.__peer_ip, self.__peer_port))
+        self.__client_socket.connect((self.__peer_ip, port))
 
         # Select loop for listen
         while self.__running == True:
@@ -109,20 +112,20 @@ if __name__ == "__main__":
     ip_addr = input('What IP (or type listen)?:')
 
     if ip_addr == 'listen':
-        server_peer = ServerPeer(1776, '')
-        client_peer = ClientPeer(1776, None)
+        server_peer = ServerPeer()
+        client_peer = ClientPeer()
         server_peer.start()
         play_input = PlayInput(client_peer, server_peer)
         play_input.start()
     elif ip_addr == 'Listen':
-        server_peer = ServerPeer(1776, '')
-        client_peer = ClientPeer(1776, None)
+        server_peer = ServerPeer()
+        client_peer = ClientPeer()
         server_peer.start()
         play_input = PlayInput(client_peer, server_peer)
         play_input.start()
     else:
-        server_peer = ServerPeer(1776, '')
-        client_peer = ClientPeer(1776, ip_addr)
+        server_peer = ServerPeer()
+        client_peer = ClientPeer()
         client_peer.__peer_ip = ip_addr
         play_input = PlayInput(client_peer, server_peer)
         client_peer.start()
